7d0c013300ef60486bb3ee72864045f293176e0c
Mikhail Shcheglov <Miha1lAleksandrov1ch@yandex.ru>
2023-09-19 23:29:00 +0300

    Проектная работа 15

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -23,7 +23,7 @@ module.exports.createCard = (req, res, next) => {
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((card) => {return res.send(card)})
+    .then((card) => res.send(card))
     .catch(next);
 };
 
